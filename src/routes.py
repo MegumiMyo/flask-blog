9ad384f21fb830f17c8ac6012ecff051f164fa0f
megumimyo <yasouo@proton.me>
2024-04-21 01:03:38 +0530

    add funtion update and delete in post

--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask import Blueprint, render_template, redirect, url_for, flash, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
 from src.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from src import db, bcrypt
@@ -123,3 +123,44 @@ def new_post():
     return render_template(
         "create_post.html", title="New Post", form=form, legend="Add Post"
     )
+
+
+@bp.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+
+
+@bp.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated!", "success")
+        return redirect(url_for("main.post", post_id=post.id))
+
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template(
+        "edit_post.html", title="Update Post", form=form, legend="Update Post"
+    )
+
+
+@bp.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted!", "success")
+    return redirect(url_for("main.home"))
